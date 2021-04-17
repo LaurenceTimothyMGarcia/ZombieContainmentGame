a739887da9aad48a71708ece075d547e1db68c74
EmergencyPlayer <EmergencyPlayer@users.noreply.github.com>
2021-04-17 10:21:07 -0700

    Trying to add pistol animation
    Trying to add pistol animation

--- Zombie Containment Game (Name in works)/Assets/Scripts/pistola.cs
+++ Zombie Containment Game (Name in works)/Assets/Scripts/pistola.cs
@@ -2,31 +2,60 @@
 
 public class pistola : MonoBehaviour
 {
+    private Animator anim;
+
     public float damage = 10f;
     public float range = 100f;
-    public float fireRate = 15f;
+    public float fireRate = 1f;
     public float impactForce = 30f;
+
+    public int ammoMax = 6;
+    public int ammoCurrent;
 
     public Camera fpsCam;
     //public ParticleSystem muzzleFlash;
 
-    private float nextTimeToFire = 0f;
+    private float nextTimeToFire;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        ammoCurrent = ammoMax;
+    }
 
     void Update()
     {
-        if(Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+        if(Input.GetButtonDown("Fire1") && ammoCurrent != 0) //&& Time.time >= nextTimeToFire)
         {
-            nextTimeToFire = Time.time + 1f / fireRate;
+            //nextTimeToFire = Time.time + 1f / fireRate;
+            anim.SetBool("Fire", true);
             Shoot();
+        }
+        else
+        {
+            anim.SetBool("Fire", false);
+        }
+
+        if(Input.GetKey("r"))
+        {
+            ammoCurrent = ammoMax;
+        }
+
+        if (nextTimeToFire < fireRate)
+        {
+            nextTimeToFire += Time.deltaTime;
         }
     }
 
     void Shoot()
     {
-       //muzzleFlash.Play();
+        //muzzleFlash.Play();
 
-       RaycastHit hit;
-       if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        if(nextTimeToFire < fireRate)
+            return;
+
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
 
@@ -41,5 +70,8 @@ public class pistola : MonoBehaviour
                 hit.rigidbody.AddForce(-hit.normal * impactForce);
             }
         }
+
+        nextTimeToFire = 0.0f;
+        ammoCurrent--;
     }
 }
