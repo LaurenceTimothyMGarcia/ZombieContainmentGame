8c2c323ceb38bd2e8a16b723dfa270fda4a93ba8
EmergencyPlayer <EmergencyPlayer@users.noreply.github.com>
2021-05-09 14:51:07 -0700

    Implemented Jonah's Spawner
    Implemented Jonah's Spawner

---
+++ Zombie Containment Game (Name in works)/Assets/Scripts/BoxSpawner.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    //Add the game object
+    public GameObject item;
+    
+    //Locations
+    private int xPos;
+    private int zPos;
+
+    //Spawner range minimums
+    public int xMin;
+    public int zMin;
+
+    //User inputs the item spawner range maximums
+    public int xMax;
+    public int zMax;
+
+    //Reduce the amount of items that spawn, the higher itemCount is
+    public int itemCount;
+    int i = 0;
+
+    void Start()
+    {
+        StartCoroutine(ItemDrop());
+    }
+
+    IEnumerator ItemDrop()
+    {
+        while(i < itemCount)
+        {
+            xPos = Random.Range(xMin, xMax); //Default: xPos = (1, 50)
+            zPos = Random.Range(zMin, zMax); //Default: zPos = (1, 31)
+
+            //Spawn item
+            Instantiate(item, new Vector3(xPos, 43, zPos), Quaternion.identity);
+
+            //Spawn delay
+            yield return new WaitForSeconds(0.5f);
+            
+            //Item spawn incriment
+            i++;
+        }
+    }
+
+}
